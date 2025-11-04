7a42c5bc7039945e4020e18bdd4d0271aaf2e36d
tohatsuwahyu <tohatsu.wbp@gmail.com>
2025-11-04 17:44:39 +0900

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,4 @@
+// GANTI dengan URL Web App dari Apps Script (Deploy → Web app)
+window.APP_CONFIG = {
+ENDPOINT: 'https://script.google.com/macros/s/AKfycbxSpIcIe1jE4_0Yo-M5jHWRonD6uw3hSr0jMuISopU4Ir2Haa70uwGGRbUNF9_1Ye-V_Q/exec'
+};
