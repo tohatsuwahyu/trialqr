73668d0cace212fa0effd2521b22e8af47878bb8
tohatsuwahyu <tohatsu.wbp@gmail.com>
2025-11-05 13:41:01 +0900

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-ENDPOINT: 'https://script.google.com/macros/s/AKfycbxSpIcIe1jE4_0Yo-M5jHWRonD6uw3hSr0jMuISopU4Ir2Haa70uwGGRbUNF9_1Ye-V_Q/exec'
+ENDPOINT: 'https://script.google.com/macros/s/AKfycbyDP5HsgOKpl5Nio5j7XSP0ciuY563K5p2PPJcdQNDKzhF8Q1XyZnyfIh8e8-yIL01F0A/exec'
 };
